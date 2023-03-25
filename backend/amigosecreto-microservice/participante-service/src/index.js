f1b91d672f728fc08c6efc1c63cf34803ebc6116
GustavoHSB <125306437+GustavoHSB@users.noreply.github.com>
2023-03-24 21:22:19 -0300

    Final

--- backend/amigosecreto-microservice/participante-service/src/index.js
+++ backend/amigosecreto-microservice/participante-service/src/index.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes')
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/:nome', (request, response) => {
+app.use(routes);
+
+mongoose.connect('',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+/*app.get('/:nome', (request, response) => {
     return response.send("Olá Mundo " + `${request.params.nome}`);
 });
 
-/*app.post('participante',(request , response) =>{
+app.post('/participante',(request , response) =>{
     console.log(request.body);
     const { nome } = request.body;
     return response.json({"mensagem":`Olá mundo $ {nome}`});
